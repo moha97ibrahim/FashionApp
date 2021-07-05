1383e15bd8e71a32f2abe00e7fb31918574a9589
mohammed.ibrahim <mohammed.ibrahim@kydonlsg.com>
2021-07-05 23:00:17 +0530

    Initial Commit

---
+++ Fashion App/Fashion_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fashion_AppApp.swift
+//  Fashion App
+//
+//  Created by Mohammed Ibrahim on 5/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Fashion_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
